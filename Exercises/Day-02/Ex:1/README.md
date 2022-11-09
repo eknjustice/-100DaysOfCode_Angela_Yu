7e5194cf7ff3e039ff07970a76b784e710d4edf8
eknjustice <eknjustice@gmail.com>
2022-11-09 13:07:19 +0200

    data_types

---
+++ Exercises/Day-02/Ex:1/README.md
@@ -0,0 +1,5 @@
+Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+Hint
+Try to find out the data type of two_digit_number.
+Think about what you learnt about subscripting.
+Think about type conversion.
---
+++ Exercises/Day-02/Ex:1/dataTypes.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+integer1 = int(two_digit_number[0])
+integer2 = int(two_digit_number[1])
+print(integer1 + integer2)
